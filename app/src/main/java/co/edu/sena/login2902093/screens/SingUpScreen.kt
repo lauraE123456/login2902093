75c12cf751a9d7e6e9126d591a28bdb5d038d433
davixTech420 <afsil6236@misena.edu.co>
2024-07-22 10:57:40 -1200

    docs:Se agrego una columna componible,, y se agrego el nombre de compasable y lo definimos con un recurso de cadena

--- app/src/main/java/co/edu/sena/login2902093/screens/SingUpScreen.kt
+++ app/src/main/java/co/edu/sena/login2902093/screens/SingUpScreen.kt
@@ -1,23 +1,34 @@
 package co.edu.sena.login2902093.screens
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import co.edu.sena.login2902093.R
+import co.edu.sena.login2902093.components.HeadingTextComponent
 import co.edu.sena.login2902093.components.NormalTextComponent
 
 
 @Composable
 fun SingUpScreen () {
     Surface (
-        color = Color.White,
-        modifier = Modifier.fillMaxSize()
-        ) { 
-            NormalTextComponent(value = stringResource(id = R.string.hello))
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+            .padding(28.dp)
+        ) {
+            Column (modifier = Modifier.fillMaxSize()){
+                NormalTextComponent(value = stringResource(id = R.string.hello))
+                HeadingTextComponent(value = stringResource(id = R.string.Create_account))
+
+            }
     }
 }
 
